7607b635b1608e2359aae9f5cab970207c418843
Hilmaww <hilmi.ambari27@gmail.com>
2020-08-11 22:40:50 +0800

    buat list notes

---
+++ src/components/listNotes.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="listNotes">
+        <ul>
+            <li v-for="row in notes">
+                {{row.title}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script type="text/javascript">
+
+    exports : default{
+        name= 'listNotes',
+        data : function(){
+            return{
+                notes : [
+                    {title : 'wegodev',
+                      description : 'ini isi wegodev'}
+                ]
+
+            }
+        }
+    }
+
+</script>
